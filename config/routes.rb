92116d8430234abf052e9b8f577f7b0016d04719
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-15 13:27:59 +0100

    Edit added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # List
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # View details
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
 end
